d57661f57b54bc0c7fc2e4df826f2d86df6ef398
SWARAJ <swarajvanchan@gmail.com>
2019-08-21 23:54:57 +0530

    Reverse a word

---
+++ src/ReverseWord.java
@@ -0,0 +1,29 @@
+/**
+ * Created by SWATHI on 8/21/2019.
+ */
+import java.util.Scanner;
+public class ReverseWord {
+    public static String reverseTheSentence(String inputString)
+    {
+        String[] words=inputString.split("\\s");
+        String outputString=" ";
+        for(int i=words.length-1;i>=0;i--)
+        {
+            outputString=outputString+words[i]+" ";
+        }
+        return outputString;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the input string ");
+        String inputString=sc.nextLine();
+        String outputString=reverseTheSentence(inputString);
+        System.out.println("inputString:"+inputString);
+        System.out.println("output String:"+outputString);
+
+
+    }
+
+}
+
+
